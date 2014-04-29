f96acd7509c32be2381747f6a86f968956924150
Jonas Gastal <gastal@noobfusion.com>
2014-04-28 22:44:07 -0300

    typo fix, really....

--- main.go
+++ main.go
@@ -7,9 +7,9 @@ import (
 )
 
 func hello(resp http.ResponseWriter, req *http.Request) {
-	fmt.Println(res, "<html><head><title>How about them apples?!</title></head><body><h1>")
-	fmt.Println(res, "Hello world!")
-	fmt.Println(res, "</h1></body></html>")
+	fmt.Println(resp, "<html><head><title>How about them apples?!</title></head><body><h1>")
+	fmt.Println(resp, "Hello world!")
+	fmt.Println(resp, "</h1></body></html>")
 }
 
 func main() {
