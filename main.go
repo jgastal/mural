87e7321f061611e6f945ff3f2b090f398eda97ab
Jonas Gastal <gastal@noobfusion.com>
2014-04-29 23:48:56 -0300

    Allow saving messages.

--- main.go
+++ main.go
@@ -9,13 +9,30 @@ import (
 )
 
 func home(resp http.ResponseWriter, req *http.Request) {
-	conn_params, err := pq.ParseURL(os.Getenv("DATABASE_URL"))
-	if err != nil {
-		panic(err)
-	}
-	conn, err := sql.Open("postgres", conn_params)
-	if err != nil {
-		panic(err)
+	if req.Method == "POST" {
+		conn_params, err := pq.ParseURL(os.Getenv("DATABASE_URL"))
+		if err != nil {
+			panic(err)
+		}
+		conn, err := sql.Open("postgres", conn_params)
+		if err != nil {
+			panic(err)
+		}
+
+		name := req.FormValue("name")
+		if name == "" {
+			name = "Anonymous coward"
+		}
+		message := req.FormValue("message")
+		if message == "" {
+			message = "Gosh, nothing to say?!"
+		}
+		_, err = conn.Exec("INSERT INTO posts (name, message) VALUES ($1, $2)", name, message)
+		if err != nil {
+			panic(err)
+		}
+
+		conn.Close()
 	}
 
 	home, err := template.ParseFiles("home.html")
@@ -27,13 +44,9 @@ func home(resp http.ResponseWriter, req *http.Request) {
 		"title": "How about them apples?!",
 		"envs":  os.Environ(),
 	}
-	if conn.Ping() == nil {
-		ctx["ps"] = "I connected to the database! YAY!"
-	} else {
-		ctx["ps"] = "I can't connect to the database! WTF?!"
-	}
 
 	home.Execute(resp, ctx)
+	
 }
 
 func main() {
