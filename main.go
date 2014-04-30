fb98c79fff2c952545187148b58cdc40c7edf3a8
Jonas Gastal <gastal@noobfusion.com>
2014-04-30 15:28:48 -0300

    Use mongodb instead of postgres.

--- main.go
+++ main.go
@@ -1,9 +1,8 @@
 package main
 
 import (
-	"database/sql"
-	"github.com/lib/pq"
 	"html/template"
+	"labix.org/v2/mgo"
 	"net/http"
 	"os"
 )
@@ -14,25 +13,23 @@ type message struct {
 }
 
 func home(resp http.ResponseWriter, req *http.Request) {
-	conn_params, err := pq.ParseURL(os.Getenv("DATABASE_URL"))
+	session, err := mgo.Dial(os.Getenv("MONGOHQ_URL"))
 	if err != nil {
 		panic(err)
 	}
-	conn, err := sql.Open("postgres", conn_params)
-	if err != nil {
-		panic(err)
-	}
+	db := session.DB("")
+	collection := db.C("posts")
 
 	if req.Method == "POST" {
 		name := req.FormValue("name")
 		if name == "" {
 			name = "Anonymous coward"
 		}
-		message := req.FormValue("message")
-		if message == "" {
-			message = "Gosh, nothing to say?!"
+		msg := req.FormValue("message")
+		if msg == "" {
+			msg = "Gosh, nothing to say?!"
 		}
-		_, err = conn.Exec("INSERT INTO posts (name, message) VALUES ($1, $2)", name, message)
+		err = collection.Insert(message{name, msg})
 		if err != nil {
 			panic(err)
 		}
@@ -45,21 +42,14 @@ func home(resp http.ResponseWriter, req *http.Request) {
 
 	var messages [10]message
 	msg_count := 0
-	rows, err := conn.Query("SELECT name, message FROM posts ORDER BY id DESC LIMIT 10");
-	if err != nil {
-		panic(err)
-	}
-	for rows.Next() {
-		var name, msg string
-		err = rows.Scan(&name, &msg)
-		if err != nil {
-			//TODO log error
-			continue
-		}
-		messages[msg_count] = message{name, msg}
+	iter := collection.Find(nil).Batch(10).Iter()
+	for iter.Next(&messages[msg_count]) && msg_count < 10 {
 		msg_count += 1
 	}
-	conn.Close()
+	if err := iter.Close(); err != nil {
+		panic(err)
+	}
+	session.Close()
 
 	ctx := map[string]interface{}{
 		"title": "How do you like them apples?!",
